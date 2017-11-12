a4b93d80f00c442e531c07929a3f788924ceabc8
Fs <Fsu0413@users.noreply.github.com>
2017-11-12 21:15:29 +0800

    Prepare to rename QMdmm* to Mdmm* because we are not using Qt here currently.

--- QMdmmServer/qmdmmsocket.h
+++ QMdmmServer/qmdmmsocket.h
@@ -2,12 +2,22 @@
 #define QMDMMSOCKET_H
 
 #include "qmdmmserverglobal.h"
+#include <QMdmmCore/QMdmmProtocol>
+#include <string>
+
+using std::string;
 
 class QMDMMSERVER_EXPORT QMdmmSocket
 {
 public:
     QMdmmSocket();
     virtual ~QMdmmSocket();
+
+    bool request(QMdmmProtocol::QMdmmRequestId requestId, const string &requestData, string &replyData);
+    void notify(QMdmmProtocol::QMdmmNotifyId notifyId, const string &notifyData);
+    void notified(QMdmmProtocol::QMdmmNotifyId notifyId, const string &notifyData);
+
+    virtual bool sendPacket(/* ??? */) = 0;
 };
 
 #endif // QMDMMSOCKET_H
