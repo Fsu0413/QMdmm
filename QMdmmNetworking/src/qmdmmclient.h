9055a38e771566eace2cfb8f204abe618f0fcb73
Fs <Fsu0413@vip.qq.com>
2024-10-07 15:58:24 +0800

    changes

--- QMdmmNetworking/src/qmdmmclient.h
+++ QMdmmNetworking/src/qmdmmclient.h
@@ -17,6 +17,25 @@ public:
     explicit QMdmmClient(QObject *parent = nullptr);
     ~QMdmmClient() override;
 
+signals:
+    void requestStoneScissorsCloth(const QStringList &playerNames, int strivedOrder);
+    void requestActionOrder(const QList<int> &remainedOrders, int maximumOrder, int selectionNum);
+    void requestAction(int currentOrder);
+    void requestUpgrade(int remainingTimes);
+
+    void notifyPlayerAdded(const QString &playerName, const QString &screenName, const QMdmmData::AgentState &agentState);
+    void notifyPlayerRemoved(const QString &playerName);
+    void notifyGameStart();
+    void notifyRoundStart();
+    void notifyStoneScissorsCloth(const QHash<QString, QMdmmData::StoneScissorsCloth> &ssc);
+    void notifyActionOrder(const QHash<int, QString> &actionOrderResult);
+    void notifyAction(const QString &playerName, QMdmmData::Action action, const QString &toPlayer, int toPlace);
+    void notifyRoundOver();
+    void notifyUpgrade(const QHash<QString, QList<QMdmmData::UpgradeItem>> &upgrades);
+    void notifyGameOver(const QStringList &winners);
+    void notifySpoken();
+    void notifyOperated();
+
 private:
     QMdmmClientPrivate *const d;
     Q_DISABLE_COPY_MOVE(QMdmmClient);
