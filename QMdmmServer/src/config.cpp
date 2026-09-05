ca6230d3aa7d6a2a754cb726d9272334edfcb704
Fs <Fsu0413@vip.qq.com>
2026-09-06 07:26:19 +0800

    QMdmmServer CLI: fix QString::arg usage
    QString::arg() expects QString instead of qPrintable

--- QMdmmServer/src/config.cpp
+++ QMdmmServer/src/config.cpp
@@ -151,10 +151,10 @@ Config::Config()
     parser.process(*qApp);
 
     if (!parser.unknownOptionNames().isEmpty())
-        configError(QStringLiteral("Unknown option: %1"), qPrintable(parser.unknownOptionNames().join(QStringLiteral(", "))));
+        configError(QStringLiteral("Unknown option: %1"), parser.unknownOptionNames().join(QStringLiteral(", ")));
 
     if (!parser.positionalArguments().isEmpty())
-        configError(QStringLiteral("Unknown argument: %1"), qPrintable(parser.positionalArguments().join(QStringLiteral(", "))));
+        configError(QStringLiteral("Unknown argument: %1"), parser.positionalArguments().join(QStringLiteral(", ")));
 
     if (parser.isSet(QStringLiteral("h"))) {
         std::cout << qPrintable(helpText) << std::flush;
